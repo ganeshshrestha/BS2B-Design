50e5de638b50cdc246980c605d9018fbc9bea0fd
ganeshshrestha <ganeshshrestha@olivemedia.co>
2017-01-20 13:32:10 +0545

    update
    update

---
+++ include/footer.php
@@ -0,0 +1,63 @@
+
+<footer id="footer">
+  <div class="container">
+
+  <div class="ft-contact">
+<h2>Contact Us</h2>
+<form>
+<div class="row">
+
+<div class="col-lg-6">
+<input class="form-control" type="text" name="" value="" placeholder="">
+</div>
+<div class="col-lg-6">
+<input class="form-control" type="text" name="" value="" placeholder="">
+</div>
+<div class="col-lg-6">
+<input class="form-control" type="text" name="" value="" placeholder="">
+</div>
+<div class="col-lg-6">
+<input class="form-control" type="text" name="" value="" placeholder="">
+</div>
+<div class="col-lg-12">
+<textarea class="form-control"></textarea>
+</div>
+</div>
+</form>
+</div>
+  </div>
+
+
+
+</footer>
+
+ <div class="copy_write">
+ <div class="ft-nav">
+<ul>
+<li><a href="#">How IT Works</a></li>
+<li><a href="#">Testimonials</a></li>
+<li><a href="#">News</a></li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">Contact</a></li>
+  </ul>
+  </div>
+  <div class="ft-social-link">
+<ul>
+<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+</ul>
+</div>
+ <p>Copyright &copy 2017 BS2B. All rights reserved.</p></div>
+
+<a href="javascript:void(0)" class="scrollup">&nbsp;</a>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scroll-page.js"></script> 
+<!-- custom js -->
+<script src="js/olive-custom.js"></script> 
+
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>BS2B</title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+   <!-- Owl Carousel Assets -->
+    <link href="owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="owl-carousel/owl.theme.css" rel="stylesheet">
+<!-- custom -->
+<link href="css/olive-custom.css" rel="stylesheet">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+<header id="main-heade">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12"><a href="#"><img src="images/logo.png" alt=""/></a></div>
+      <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        <div class="nav-search">
+          <form>
+            <input type="text" class="tsearch" name="" placeholder="Seach here .."/>
+            <button type="submit" class="btn-search">Search <i class="fa fa-search"></i> </button>
+          </form>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12">
+
+      
+        <div class="nav-bars"> <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span> </div>
+
+        <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&#9776;</a>
+
+  <ul>
+<li><a href="#"><i class="fa fa-indent" aria-hidden="true"></i> How IT Works</a></li>
+<li><a href="#"><i class="fa fa-comments-o" aria-hidden="true"></i> Testimonials</a></li>
+<li><a href="#"><i class="fa fa-file-text-o" aria-hidden="true"></i> News</a></li>
+<li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i> Blog</a></li>
+<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
+  </ul>
+  
+
+</div>
+      </div>
+    </div>
+  </div>
+</header>
